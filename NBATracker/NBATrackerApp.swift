5c77567bdefcab631c186456019f65bbcdd5c6d8
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2021-06-13 19:23:17 -0400

    Initial Commit

---
+++ NBATracker/NBATrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBATrackerApp.swift
+//  NBATracker
+//
+//  Created by Camille Bourbonnais on 2021-06-13.
+//
+
+import SwiftUI
+
+@main
+struct NBATrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
