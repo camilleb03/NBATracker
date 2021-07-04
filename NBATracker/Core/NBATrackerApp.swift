993dad3ceaf934faedd9345ffea663f4061b1d6e
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2021-07-04 12:11:01 -0400

    CB (F) : Added temporary standings in main tab

--- NBATracker/Core/NBATrackerApp.swift
+++ NBATracker/Core/NBATrackerApp.swift
@@ -10,12 +10,21 @@ import SwiftUI
 @main
 struct NBATrackerApp: App {
     
-    @State var showLaunchView: Bool = true
+    @State private var showLaunchView: Bool = true
+    @State private var selection = 1
     
     var body: some Scene {
         WindowGroup {
             ZStack {
-                TabView {
+                TabView(selection: $selection) {
+                    
+                    Text("League Standings")
+                    .tabItem {
+                        Label("Standings", systemImage: "list.number")
+                            .foregroundColor(Color.theme.accent)
+                    }
+                    .tag(0)
+                    
                     NavigationView {
                         ScoreboardView()
                     }
@@ -24,14 +33,14 @@ struct NBATrackerApp: App {
                         Label("Games", systemImage: "sportscourt")
                             .foregroundColor(Color.theme.accent)
                     }
-                    .tag(0)
+                    .tag(1)
                     
                     SearchLeagueView()
                     .tabItem {
                         Label("Search", systemImage: "magnifyingglass")
                             .foregroundColor(Color.theme.accent)
                     }
-                    .tag(1)
+                    .tag(2)
                 }
                 
                 ZStack {
